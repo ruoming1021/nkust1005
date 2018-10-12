973f7e6660cd4117fb097567875388ce16c6016f
Hong Louis <mm9301tw@gmail.com>
2018-10-12 11:26:16 +0800

    add

---
+++ opendata/opendatanew/opendatanew/model/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opendata.model
+{
+    class Class1
+    {
+        public int id { get; set; }
+        public string 資料集名稱 { get; set; }
+        public string 服務分類 { get; set; }
+    }
+}
